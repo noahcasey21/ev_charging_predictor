eacacbdd62c0d8f25585061f1ac325d561d66438
Noah Casey <nccm2d@umsystem.edu>
2024-10-29 22:13:24 -0500

    Added noah_s.py predictions to visualization and tested

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import orjson
 import json
 from algos.frank import choose_new_location as frank_choose_new_location
 from algos.noah_c import choose_new_location as noah_choose_new_location
+from algos.noah_s import choose_new_location_kmeans 
 
 app = Flask(__name__)
 cache = Cache(app, config={'CACHE_TYPE': 'SimpleCache'})
@@ -50,13 +51,17 @@ def run_model() -> Response:
     #post to local file that can be detected and run by javascript
     data = request.json['filtered_station_data']
     existing_locations = [(entry[0], entry[1]) for entry in data]
+
     frank_pred = frank_choose_new_location(existing_locations)
-    noah_pred = noah_choose_new_location(existing_locations)
+    noah_c_pred = noah_choose_new_location(existing_locations)
+    noah_s_pred = choose_new_location_kmeans(existing_locations)
 
     predictions = {
         'Frank' : [round(float(frank_pred[0]), 5), round(float(frank_pred[1]), 5)],
-        'Noah C' : [round(float(noah_pred[0]), 5), round(float(noah_pred[1]), 5)]
+        'Noah C' : [round(float(noah_c_pred[0]), 5), round(float(noah_c_pred[1]), 5)],
+        'Noah S' : [round(float(noah_s_pred[0]), 5), round(float(noah_s_pred[1]), 5)]
         }
+    
     pred_json = orjson.dumps(predictions)
     return Response(pred_json, content_type='application/json')
 
